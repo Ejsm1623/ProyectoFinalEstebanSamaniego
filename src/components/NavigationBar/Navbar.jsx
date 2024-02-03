87b8e6736c35534ffb1bc9a663b13407866faf15
EjsmUIUX <ejsm1623@gmail.com>
2024-02-03 15:00:56 -0500

    Rebuild de Package-lock.json

--- src/components/NavigationBar/Navbar.jsx
+++ src/components/NavigationBar/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "./css/navbar.css";
 import CartButton from "../Buttons/Cart/CartBtn.jsx"; // Importa el componente CartButton
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../logo.jsx";
 
 export default function NavBar() {
+  const currentLocation = useLocation();
+  const noRenderRoute = location.pathname === "/";
+
   return (
     <header className="container-fluid nav-container">
       <nav className="navbar navbar-expand-lg bg-body-light" id="custom-navbar">
         <Link to={"/"} className="navbar-brand d-md-block d-lg-none" href="#">
-          <span className="navbar-title" id="navbar-brandtitle">CraftMania Shop</span>
+          <span className="navbar-title" id="navbar-brandtitle">
+            CraftMania Shop
+          </span>
         </Link>
         <button
           className="navbar-toggler"
@@ -22,7 +28,7 @@ export default function NavBar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto text-uppercase fw-light">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0 mx-auto text-uppercase fw-light align-items-center">
             <li className="nav-item">
               <Link
                 className="nav-link active mx-3"
@@ -37,25 +43,19 @@ export default function NavBar() {
                 Catalogo
               </Link>
             </li>
-            <li className="nav-item">
-              <a className="nav-link mx-3">
-                Sobre Nosotros
-              </a>
+            {/* {!noRenderRoute && (
               <li>
+                <Logo />
               </li>
+            )} */}
+            <li className="nav-item">
+              <a className="nav-link mx-3">Entregas & Pedidos</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link mx-3">
-                Entregas & Pedidos
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link mx-3">
-                Contacto
-              </a>
+              <a className="nav-link mx-3">Contacto</a>
             </li>
           </ul>
-          <CartButton />
+          {/* <CartButton /> */}
         </div>
       </nav>
     </header>
